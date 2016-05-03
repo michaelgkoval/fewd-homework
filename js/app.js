eb652f5f4a9ed16135d6b125f95e1cebdb05785f
Michael Koval <michael.koval@nfl.com>
2016-05-03 14:17:55 -0700

    js fix
    js fix

--- js/app.js
+++ js/app.js
@@ -2,8 +2,4 @@ $(document).ready(function() {
   $('header nav ul li:first-child').click(function(){
     $(this).parent().toggleClass('slideDown');
   });
-  $('header nav ul li').click(function(){
-    $(this).parent().toggleClass('slideUp');
-  });
-});
 });
