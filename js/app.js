f0dfd3feff9637b0b3e08a6e4ec297d2c95be5e4
Michael Koval <michael.koval@nfl.com>
2016-05-03 13:52:01 -0700

    update
    folder updating 2.0

--- js/app.js
+++ js/app.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
   $('header nav ul li:first-child').click(function(){
     $(this).parent().toggleClass('slideDown');
   });
+  $('header nav ul li').click(function(){
+    $(this).parent().toggleClass('slideUp');
+  });
+});
 });
