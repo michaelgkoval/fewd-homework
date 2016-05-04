b9e5a5c0a789acc1cd84de6243ef09498f2d4f7f
Michael Koval <michael.koval@nfl.com>
2016-05-03 17:26:41 -0700

    add alert
    alert

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
 $(document).ready(function() {
+
+//targets the the hamburger id in the html and has the hamburger menu slide doown//
   $('header nav ul li:first-child').click(function(){
     $("#hamburger").toggleClass("slideDown");
   });
+//targets the a tags in the hamurger menu when clicked it remove the slide down class.//
   $('header nav ul li a').click(function(){
-    console.log("hello world");
     $("#hamburger").removeClass('slideDown');
+  });
+//if the user clicks on the hero image it will trigger my favorite product manager quote.//
+  $( "#hero" ).click(function() {
+      alert( "A great product manager has the brain of an engineer, the heart of a designer, and the speech of a diplomat. - Deep Nishar, UX at Linkedin" );
     });
+
 });
